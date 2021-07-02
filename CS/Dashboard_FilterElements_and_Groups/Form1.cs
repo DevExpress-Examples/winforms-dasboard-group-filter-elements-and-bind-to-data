885cc4526537859f825a55acfc7e30f5980179d0
Natalia Kazakova <17986517+natakazakova@users.noreply.github.com>
2021-07-02 17:27:04 +0300

    Update Form1.cs

--- CS/Dashboard_FilterElements_and_Groups/Form1.cs
+++ CS/Dashboard_FilterElements_and_Groups/Form1.cs
@@ -1,26 +1,22 @@
-﻿using DevExpress.DashboardCommon;
+using DevExpress.DashboardCommon;
 using DevExpress.DataAccess.ConnectionParameters;
 using DevExpress.DataAccess.Sql;
 using DevExpress.XtraEditors;
 using System;
 
-namespace Dashboard_FilterElements_and_Groups
-{
+namespace Dashboard_FilterElements_and_Groups {
     public partial class Form1 : XtraForm {
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e) {
             DashboardSqlDataSource dataSource = CreateDataSource();
             dashboardViewer1.Dashboard = CreateDashboard(dataSource);
             dashboardViewer1.SetMasterFilter("comboBoxDashboardItem1", "Internet Explorer");
         }
 
-        private static DashboardSqlDataSource CreateDataSource()
-        {
+        private static DashboardSqlDataSource CreateDataSource() {
             DashboardSqlDataSource dataSource = new DashboardSqlDataSource();
             dataSource.ConnectionParameters =
                 new XmlFileConnectionParameters(@"..\..\Data\WebsiteStatisticsData.xml");
@@ -31,13 +27,12 @@ namespace Dashboard_FilterElements_and_Groups
             dataSource.Queries.Add(selectQuery);
             return dataSource;
         }
-        private static Dashboard CreateDashboard(DashboardSqlDataSource dataSource)
-        {
+        
+        private static Dashboard CreateDashboard(DashboardSqlDataSource dataSource) {
             Dashboard dashboard1 = new Dashboard();
             dashboard1.DataSources.Add(dataSource);
 
-            ComboBoxDashboardItem comboBox1 = new ComboBoxDashboardItem()
-            {
+            ComboBoxDashboardItem comboBox1 = new ComboBoxDashboardItem() {
                 Name = "Browser",
                 DataSource = dataSource,
                 DataMember = "Statistics",
@@ -45,8 +40,7 @@ namespace Dashboard_FilterElements_and_Groups
             };
             comboBox1.FilterDimensions.Add(new Dimension("Browser"));
 
-            ListBoxDashboardItem listBox1 = new ListBoxDashboardItem()
-            {
+            ListBoxDashboardItem listBox1 = new ListBoxDashboardItem() {
                 Name = "Browser Version",
                 DataSource = dataSource,
                 DataMember = "Statistics"
@@ -54,8 +48,7 @@ namespace Dashboard_FilterElements_and_Groups
             listBox1.FilterDimensions.Add(new Dimension("BrowserDetails"));
             listBox1.InteractivityOptions.IgnoreMasterFilters = false;
 
-            TreeViewDashboardItem treeView1 = new TreeViewDashboardItem()
-            {
+            TreeViewDashboardItem treeView1 = new TreeViewDashboardItem() {
                 Name = "Date",
                 DataSource = dataSource,
                 DataMember = "Statistics",
@@ -64,16 +57,14 @@ namespace Dashboard_FilterElements_and_Groups
             treeView1.FilterDimensions.AddRange(new Dimension("Date", DateTimeGroupInterval.Year),
                 new Dimension("Date", DateTimeGroupInterval.Quarter));
 
-            DashboardItemGroup group1 = new DashboardItemGroup()
-            {
+            DashboardItemGroup group1 = new DashboardItemGroup() {
                 Name = "Filters"
             };
             group1.InteractivityOptions.IsMasterFilter = true;
             dashboard1.Groups.Add(group1);
             group1.AddRange(comboBox1, treeView1, listBox1);
 
-            ChartDashboardItem chart1 = new ChartDashboardItem()
-            {
+            ChartDashboardItem chart1 = new ChartDashboardItem() {
                 Name = "Browser Statistics",
                 DataSource = dataSource,
                 DataMember = "Statistics",
